c7cf56eb6753635581a1f45b2c12e474fc538af1
mreb6832-cpu <mreb6832@gmail.com>
2026-09-26 12:12:03 +0200

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,13 +1,9 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL =
-  https://akbictykozgaeuvbqbjl.supabase.co
-  
-
-const SUPABASE_KEY =
-  "sb_publishable_Y4soeQaBE8hn8RazFTZPpQ_7eoaw_cB
+const supabaseUrl = "https://akbictykozgaeuvbqbjl.supabase.co";
+const supabaseKey = "sb_publishable_Y4soeQaBE8hn8RazFTZPpQ_7eoaw_cB";
 
 export const supabase = createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
+  supabaseUrl,
+  supabaseKey
 );
